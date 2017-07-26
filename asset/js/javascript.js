8845dcd8620f1106a87038beab997cebc384e017
haidy <haidy>
2017-07-26 00:27:59 -0500

    Avance

---
+++ asset/js/javascript.js
@@ -0,0 +1,87 @@
+var listaTareas = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }
+]
+
+
+ var html = "";
+ for (var i =0; i<listaTareas.length;i++){
+  var dato_1 = listaTareas[i];
+  //alert (dato_1);
+  var s = "<li>"+ dato_1.title +"</li>";
+  html += s;
+ }
+var lista = document.getElementById('list');
+lista.innerHTML = html;
+
+function add() {
+  var etiquetali = document.createElement("li");
+  var nuevaTarea = document.getElementById("newTarea").value;
+  var contenidoEtiqueta = document.createTextNode(nuevaTarea);
+  etiquetali.appendChild(contenidoEtiqueta);
+  document.getElementById("list").appendChild(etiquetali);
+  document.getElementById("newTarea").value = "";
+  var pushArray = {
+  title: nuevaTarea,
+  }
+  listaTareas.push(pushArray);
+  
+}
